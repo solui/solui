3dd20c2bbd0f52d0c0cf8714a0555e903bb4e3fa
Ramesh Nair <ram@hiddentao.com>
2019-09-07 13:08:58 +0100

    babel compilation working

--- packages/styles/src/themes.js
+++ packages/styles/src/themes.js
@@ -1,10 +1,6 @@
-const themes = [
-  'default',
-].reduce((m, s) => {
-  // eslint-disable-next-line import/no-dynamic-require
-  m[s] = require(`./themes/${s}`).default
-  return m
-}, {})
+const themes = {
+  default: require('./themes/default').default
+}
 
 export const getTheme = (s = 'default') => ({
   ...themes[s],
