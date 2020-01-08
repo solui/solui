bc8262c2c8a734e82f2bc8c7829eebfc6574af40
Ramesh Nair <ram@hiddentao.com>
2020-01-08 17:04:02 +0000

    build: build all packages prior to release

---
+++ scripts/prepare-release.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd packages/utils && yarn build
+cd ../styles && yarn build
+cd ../react && yarn build
+cd ../processor && yarn build
+cd ../react && yarn build
+cd ../cli && yarn build
+
+cd ../..
+
+lerna version --conventional-commits
+
---
+++ scripts/release.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -e
+
+lerna publish from-git
