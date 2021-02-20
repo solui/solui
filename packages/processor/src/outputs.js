b591c94d4848c4ec44855cc33deef11753117b34
t0m <husamirami@gmail.com>
2021-02-20 14:46:06 +0200

    fixed something

--- packages/processor/src/outputs.js
+++ packages/processor/src/outputs.js
@@ -1,21 +1,6 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.processList = void 0;
-
-var _utils = require("./utils");
-
-var _context = require("./context");
-
-var _transform = require("./transform");
-
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
-
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+import { resolveValue } from './utils'
+import { createChildContextFrom } from './context'
+import { transformValue } from './transform'
 
 const OUTPUT_TYPES = {
   address: true,
@@ -25,21 +10,17 @@ const OUTPUT_TYPES = {
   int: true,
   'bytes32[]': true,
   'int[]': true,
-  'address[]': true
-};
+  'address[]': true,
+}
 
 const process = (ctx, config) => {
   if (!config) {
-    return;
+    return
   }
 
-  const {
-    type,
-    value,
-    title,
-    transform = []
-  } = config;
+  const { type, value, title, transform = [] } = config
 
+  // handle deprecated properties: unit, scale
   if (config.scaleDown && config.scale) {
     
     let _scale;
@@ -82,38 +63,37 @@ const process = (ctx, config) => {
   }
 
   if (!type || !value || !title) {
-    ctx.recordError(`output type, value and title must be specified`);
+    ctx.recordError(`output type, value and title must be specified`)
   } else {
     if (!OUTPUT_TYPES[type]) {
-      ctx.recordError(`output type is not valid: ${type}`);
+      ctx.recordError(`output type is not valid: ${type}`)
     }
 
-    let result;
+    let result
 
     try {
-      result = (0, _utils.resolveValue)(ctx, value);
+      result = resolveValue(ctx, value)
     } catch (err) {
-      ctx.recordError(`output value is not valid: ${value}`);
+      ctx.recordError(`output value is not valid: ${value}`)
     }
 
-    let resultTransformed;
+    let resultTransformed
 
     if (typeof result !== 'undefined') {
-      resultTransformed = (0, _transform.transformValue)(ctx, result, transform);
+      resultTransformed = transformValue(ctx, result, transform)
     }
 
-    ctx.outputs().set(ctx.id, _objectSpread({}, config, {
+    ctx.outputs().set(ctx.id, {
+      ...config,
       result,
-      resultTransformed
-    }));
+      resultTransformed,
+    })
   }
-};
+}
 
-const processList = async (parentCtx, outputs) => {
+export const processList = async (parentCtx, outputs) => {
   return Promise.all((outputs || []).map(async (outputConfig, ouputIndex) => {
-    const ctx = (0, _context.createChildContextFrom)(parentCtx, `output[${ouputIndex}]`);
-    return process(ctx, outputConfig);
-  }));
-};
-
-exports.processList = processList;
+    const ctx = createChildContextFrom(parentCtx, `output[${ouputIndex}]`)
+    return process(ctx, outputConfig)
+  }))
+}
