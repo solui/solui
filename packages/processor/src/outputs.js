dfd07191a2438647094ad8fbdff79dd2fb3543d4
t0m <husamirami@gmail.com>
2021-02-20 14:41:45 +0200

    added scaleDown and allowed resolving reference in scale

--- packages/processor/src/outputs.js
+++ packages/processor/src/outputs.js
@@ -1,6 +1,21 @@
-import { resolveValue } from './utils'
-import { createChildContextFrom } from './context'
-import { transformValue } from './transform'
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.processList = void 0;
+
+var _utils = require("./utils");
+
+var _context = require("./context");
+
+var _transform = require("./transform");
+
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 const OUTPUT_TYPES = {
   address: true,
@@ -10,56 +25,95 @@ const OUTPUT_TYPES = {
   int: true,
   'bytes32[]': true,
   'int[]': true,
-  'address[]': true,
-}
+  'address[]': true
+};
 
 const process = (ctx, config) => {
   if (!config) {
-    return
+    return;
   }
 
-  const { type, value, title, transform = [] } = config
+  const {
+    type,
+    value,
+    title,
+    transform = []
+  } = config;
 
-  // handle deprecated properties: unit, scale
-  if (config.scale) {
-    if (config.unit) {
-      transform.unshift({ type: "stringToSpacedSuffixedString", suffix: config.unit })
+  if (config.scaleDown && config.scale) {
+    
+    let _scale;
+
+    try {
+      _scale = (0, _utils.resolveValue)(ctx, config.scale);
+    } catch (err) {
+      ctx.recordError(`output value is not valid: ${config.scale}`);
     }
-    transform.unshift({ type: "intToScaledIntString", scale: config.scale })
+
+    if (typeof _scale !== 'undefined') {
+      config.scale = _scale;
+    }
+
+    if (config.unit) {
+      transform.unshift({
+        type: "stringToSpacedSuffixedString",
+        suffix: config.unit
+      });
+    }
+
+    transform.unshift({
+      type: "intToScaledDownIntString",
+      scale: config.scale
+    });
+  }
+
+  if (config.scale && !config.scaleDown) {
+    if (config.unit) {
+      transform.unshift({
+        type: "stringToSpacedSuffixedString",
+        suffix: config.unit
+      });
+    }
+
+    transform.unshift({
+      type: "intToScaledIntString",
+      scale: config.scale
+    });
   }
 
   if (!type || !value || !title) {
-    ctx.recordError(`output type, value and title must be specified`)
+    ctx.recordError(`output type, value and title must be specified`);
   } else {
     if (!OUTPUT_TYPES[type]) {
-      ctx.recordError(`output type is not valid: ${type}`)
+      ctx.recordError(`output type is not valid: ${type}`);
     }
 
-    let result
+    let result;
 
     try {
-      result = resolveValue(ctx, value)
+      result = (0, _utils.resolveValue)(ctx, value);
     } catch (err) {
-      ctx.recordError(`output value is not valid: ${value}`)
+      ctx.recordError(`output value is not valid: ${value}`);
     }
 
-    let resultTransformed
+    let resultTransformed;
 
     if (typeof result !== 'undefined') {
-      resultTransformed = transformValue(ctx, result, transform)
+      resultTransformed = (0, _transform.transformValue)(ctx, result, transform);
     }
 
-    ctx.outputs().set(ctx.id, {
-      ...config,
+    ctx.outputs().set(ctx.id, _objectSpread({}, config, {
       result,
-      resultTransformed,
-    })
+      resultTransformed
+    }));
   }
-}
+};
 
-export const processList = async (parentCtx, outputs) => {
+const processList = async (parentCtx, outputs) => {
   return Promise.all((outputs || []).map(async (outputConfig, ouputIndex) => {
-    const ctx = createChildContextFrom(parentCtx, `output[${ouputIndex}]`)
-    return process(ctx, outputConfig)
-  }))
-}
+    const ctx = (0, _context.createChildContextFrom)(parentCtx, `output[${ouputIndex}]`);
+    return process(ctx, outputConfig);
+  }));
+};
+
+exports.processList = processList;
--- packages/processor/src/transform.js
+++ packages/processor/src/transform.js
@@ -1,57 +1,83 @@
-import {
-  _,
-  prettyDate,
-  deriveDecimalVal,
-} from '@solui/utils'
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.transformValue = void 0;
+
+var _utils = require("@solui/utils");
 
 const intToDecimal = (value, scale = 0) => {
-  const d = deriveDecimalVal(value, { scale })
-  if (!d) {
-    throw new Error('invalid value')
-  }
-  return d
-}
+  const d = (0, _utils.deriveDecimalVal)(value, {
+    scale
+  });
 
-export const transformValue = (ctx, value, transform = []) => {
-  if (!_.get(transform, 'length')) {
-    return undefined
+  if (!d) {
+    throw new Error('invalid value');
+  }
+
+  return d;
+};
+
+const transformValue = (ctx, value, transform = []) => {
+  if (!_utils._.get(transform, 'length')) {
+    return undefined;
   }
 
   transform.forEach(t => {
     try {
       switch (t.type) {
-        case 'toMappedString': {
-          value = `${t.options[`${value}`]}`
-          break
-        }
-        case 'stringToSpacedSuffixedString': {
-          value = `${value} ${t.suffix}`
-          break
-        }
-        case 'intToScaledIntString': {
-          value = intToDecimal(value, parseInt(t.scale, 10)).toString()
-          break
-        }
-        case 'intToHexString': {
-          value = intToDecimal(value).toHex()
-          break
-        }
-        case 'intToBinaryString': {
-          value = intToDecimal(value).toBinary()
-          break
-        }
-        case 'intToDateString': {
-          const d = intToDecimal(value)
-          value = prettyDate(d.mul(1000).toNumber(), t.format || 'MMM d, yyyy')
-          break
-        }
+        case 'toMappedString':
+          {
+            value = `${t.options[`${value}`]}`;
+            break;
+          }
+
+        case 'stringToSpacedSuffixedString':
+          {
+            value = `${value} ${t.suffix}`;
+            break;
+          }
+
+        case 'intToScaledIntString':
+          {
+            value = intToDecimal(value, parseInt(t.scale, 10)).toString();
+            break;
+          }
+          
+        case 'intToScaledDownIntString':
+          {
+            value = intToDecimal(value, -1 * parseInt(t.scale, 10)).toString();
+            break;
+          }
+
+        case 'intToHexString':
+          {
+            value = intToDecimal(value).toHex();
+            break;
+          }
+
+        case 'intToBinaryString':
+          {
+            value = intToDecimal(value).toBinary();
+            break;
+          }
+
+        case 'intToDateString':
+          {
+            const d = intToDecimal(value);
+            value = (0, _utils.prettyDate)(d.mul(1000).toNumber(), t.format || 'MMM d, yyyy');
+            break;
+          }
+
         default:
-          throw new Error(`invalid transform: ${t.type}`)
+          throw new Error(`invalid transform: ${t.type}`);
       }
     } catch (err) {
-      ctx.recordError(`transform failed (${t.type}): ${err.message}`)
+      ctx.recordError(`transform failed (${t.type}): ${err.message}`);
     }
-  })
+  });
+  return value;
+};
 
-  return value
-}
+exports.transformValue = transformValue;
